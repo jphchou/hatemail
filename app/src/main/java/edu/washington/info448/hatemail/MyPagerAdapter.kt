a7caf1b44b64248c6df1ebabd59755ada4db6c80
cchen97 <manjic@uw.edu>
2019-05-30 14:18:16 -0700

    added shared preference

--- app/src/main/java/edu/washington/info448/hatemail/MyPagerAdapter.kt
+++ app/src/main/java/edu/washington/info448/hatemail/MyPagerAdapter.kt
@@ -3,16 +3,26 @@ package edu.washington.info448.hatemail
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import com.google.gson.reflect.TypeToken
+import com.google.gson.Gson
+import android.preference.PreferenceManager
+import android.content.SharedPreferences
+
+
+
+
 
 class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    val histories = arrayListOf<History>(his1, his2, his3)
+    private var histories = arrayListOf(his1, his2, his3)
+    lateinit var myHistories: ArrayList<History>
+//    lateinit var myScedules: ArrayList<Schedule>
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
             0 -> {
                 FirstFragment()
             }
-            1 -> HistoryFragment.newInstance(histories)
+            1 -> HistoryFragment.newInstance(myHistories)
             else -> {
                 return HistoryFragment.newInstance(histories)
             }
